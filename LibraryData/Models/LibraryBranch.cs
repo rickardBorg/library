a585883d4318019d06c5112be373c5e95c0e184e
Rickard Borg <rickard.borg1990@gmail.com>
2019-12-30 14:36:13 +0100

    initial commit

---
+++ LibraryData/Models/LibraryBranch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryData.Models
+{
+    public class LibraryBranch
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Adress { get; set; }
+
+        [Required]
+        public string Telephone { get; set; }
+        public string Descripton { get; set; }
+        public DateTime OpenDate { get; set; }
+
+        public virtual IEnumerable<Patron> Patrons { get; set; }
+        public virtual IEnumerable<LibraryAsset> LibraryAssets { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
